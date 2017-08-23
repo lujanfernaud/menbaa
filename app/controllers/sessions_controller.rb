4013ba4530a3ba27dad306e094a6e620b0a3a105
lujanfernaud <lujanfernaud@gmail.com>
2017-08-23 20:55:09 +0100

    Implement sessions with remember option

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,26 @@
 class SessionsController < ApplicationController
   def new
+    return unless logged_in?
+
+    flash[:info] = "You are already logged in as #{current_user.name}."
+    redirect_to root_url
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember_me? ? remember(user) : forget(user)
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Invalid email or password."
+      render "new"
+    end
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   has_secure_password
+
+  def remember
+    self.remember_token = User.generate_token
+    update_attribute(:remember_digest, User.generate_digest(remember_token))
+  end
+
+  def forget
+    self.remember_token = nil
+    update_attribute(:remember_digest, nil)
+  end
+
+  def User.generate_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.generate_digest(token)
+    Digest::SHA1.hexdigest(token)
+  end
 end
