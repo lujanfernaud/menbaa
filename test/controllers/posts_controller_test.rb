80bf782d45b688249b5ed5dbc8fea17c887f6c2b
lujanfernaud <lujanfernaud@gmail.com>
2017-08-23 22:18:53 +0100

    Fix PostsController and SessionsController tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get posts_index_url
+    get root_url
     assert_response :success
   end
 
