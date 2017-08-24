9eb946b03eb5fe28e37c92512f5c7ff3171c74ae
lujanfernaud <lujanfernaud@gmail.com>
2017-08-24 07:54:34 +0100

    Add validations for :name and :email to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
 
+  validates :name,  presence: true, length: { in: 2..50 },
+                    uniqueness: { case_sensitive: false }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
   has_secure_password
 
   def remember
