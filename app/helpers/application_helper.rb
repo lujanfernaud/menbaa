e3a3456345ac4ca41a3c27837d6ff25f93bc1dc2
lujanfernaud <lujanfernaud@gmail.com>
2017-08-25 18:58:13 +0100

    Add ApplicationHelper#index?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = "Members Only"
     page_title.empty? ? base_title : base_title + " | " + page_title
   end
+
+  def index?(controller_name, action_name)
+    controller_name == "posts" && action_name == "index"
+  end
 end
